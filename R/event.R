05d572792ac7335d1699d876fb5c02f06d55d95f
Julian Grimm <grimmjulian225@gmail.com>
2023-04-23 10:46:47 +0200

    Change file structure and move functions
    Move functions from coach file to event file as they did not do any
    smart stuff.
    Split Shiny functions from backend functions in routine.R, pairing.R and
    event.R.

--- R/event.R
+++ R/event.R
@@ -49,6 +49,21 @@ methods::setMethod("as.list", "Event", function(x) {
   list(x@first, x@second, x@third, x@fourth)
 })
 
+team_routines <- function(event, team = "guest") {
+  event <- as.list(event)
+  routines <- lapply(event, slot, name = team)
+  return(routines)
+}
+
+methods::setMethod("reorder",
+                   "Event", 
+                   function(x, home_order = 1:4, guest_order = 1:4, home_starts = x@first@home_starts) {
+  home <- team_routines(x, team = "home")[home_order]
+  guest <- team_routines(x, team = "guest")[guest_order]
+  event <- Event.routines(home, guest, home_starts = home_starts)
+  return(event)
+})
+
 Event.pairings <- function(pairings) {
   stopifnot(length(pairings) == 4)
   stopifnot(all(sapply(pairings, is, class2 = "Pairing")))
@@ -63,48 +78,12 @@ Event.routines <- function(home, guest, home_starts = TRUE) {
   stopifnot(all(sapply(guest, is, class2 = "Routine")))
   home_starts <- c(home_starts, home_starts, !home_starts, !home_starts)
   pairings <- mapply(methods::new,
-                     home = home,
-                     guest = guest,
-                     home_starts = home_starts,
-                     MoreArgs = list(Class = "Pairing"))
+    home = home,
+    guest = guest,
+    home_starts = home_starts,
+    MoreArgs = list(Class = "Pairing")
+  )
   event <- Event.pairings(pairings)
   return(event)
 }
 
-eventScoreUI <- function(id) {
-  shiny::fluidRow(
-    shiny::column(3, ""),
-    shiny::column(2, "Summe"),
-    shiny::column(1, scoreUI(shiny::NS(id, "score_home"))),
-    shiny::column(3, ""),
-    shiny::column(2, "Summe"),
-    shiny::column(1, scoreUI(shiny::NS(id, "score_guest")))
-  )
-}
-
-eventResultUI <- function(id, home_starts = TRUE) {
-  shiny::tagList(
-    pairingHeaderUI(),
-    pairingResultUI(shiny::NS(id, "first")),
-    pairingResultUI(shiny::NS(id, "second")),
-    pairingResultUI(shiny::NS(id, "third")),
-    pairingResultUI(shiny::NS(id, "fourth")),
-    eventScoreUI(id),
-    shiny::checkboxInput(shiny::NS(id, "home_starts"), "Heim beginnt", TRUE)
-  )
-}
-
-eventResultServer <- function(id) {
-  shiny::moduleServer(id, function(input, output, session) {
-    event <- shiny::reactive(methods::new("Event",
-      first = pairingResultServer("first", input$home_starts)(),
-      second = pairingResultServer("second", input$home_starts)(),
-      third = pairingResultServer("third", !input$home_starts)(),
-      fourth = pairingResultServer("fourth", !input$home_starts)()
-    ))
-    s <- shiny::reactive(score(event()))
-    output$score_home <- shiny::renderText(s()[1])
-    output$score_guest <- shiny::renderText(s()[2])
-    return(event)
-  })
-}
