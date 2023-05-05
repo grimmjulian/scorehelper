997785d527cf10991431cd0a2574fd05a8f5d50c
Julian Grimm <grimmjulian225@gmail.com>
2023-05-05 13:51:54 +0200

    Add functionality to choose the first two routines

--- R/coach.R
+++ R/coach.R
@@ -3,3 +3,15 @@ permutations <- function(event) {
   lapply(orders, reorder, x = event)
 }
 
+get_first_two_pairings <- function(event, home_starts = TRUE) {
+  possible_events <- permutations(event)
+  event_ratings <- sapply(possible_events, rating)
+  event_order <- order(event_ratings, decreasing = !home_starts)
+  event <- possible_events[event_order][[1]]
+  event <- sort(event, home_starts)
+  pairings <- as.list(event)[1:2]
+  pairings[[1]]@home_starts <- TRUE 
+  pairings[[2]]@home_starts <- TRUE 
+  return(pairings)
+}
+
--- tests/testthat/test-coach.R
+++ tests/testthat/test-coach.R
@@ -2,3 +2,12 @@ test_that("permutations are correct", {
   event <- new("Event")
   expect_length(permutations(event), 24)
 })
+
+test_that("the first two pairings work", {
+            r1 <- new("Routine", endvalue = 12)
+            r2 <- new("Routine", endvalue = 14)
+            p <- new("Pairing", home = r1, guest = r2)
+            e <- new("Event", first = p)
+            pairings  <- get_first_two_pairings(e)
+            expect_equal(pairings, list(new("Pairing"), new("Pairing")))
+})
