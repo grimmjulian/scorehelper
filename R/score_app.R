d2d95ef078f27c37fc6b82fdad5ce1ebd385f0fb
Julian Grimm <grimmjulian225@gmail.com>
2023-05-13 18:29:13 +0200

    Add UI and server for score results

---
+++ R/score_app.R
@@ -0,0 +1,26 @@
+scoreUI <- function(id, team) {
+  stopifnot(team %in% c("home", "guest"))
+  shiny::tagList(
+    shiny::textOutput(shiny::NS(id, team))
+  )
+}
+
+scoreServer <- function(id, pairing) {
+  shiny::moduleServer(id, function(input, output, session) {
+    s <- shiny::reactive(score(pairing))
+    output$home <- shiny::renderText(s()[1])
+    output$guest <- shiny::renderText(s()[2])
+  })
+}
+
+scoreApp <- function(pairing = new("Pairing")) {
+  ui <- shiny::fluidPage(
+    scoreUI("x", "home"),
+    scoreUI("x", "guest")
+  )
+  server <- function(input, output, session) {
+    scoreServer("x", pairing)
+  }
+
+  shiny::shinyApp(ui, server)
+}
