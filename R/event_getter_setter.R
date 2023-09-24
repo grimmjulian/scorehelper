925b89ba6ed0a76428f6a6387f6a16338766fe54
Julian Grimm <grimmjulian225@gmail.com>
2023-09-24 09:55:40 +0200

    Write loop for creating getter methods

--- R/event_getter_setter.R
+++ R/event_getter_setter.R
@@ -1,88 +1,43 @@
+event_names <- c(
+  "floor",
+  "pommel_horse",
+  "still_rings",
+  "vault",
+  "parallel_bars",
+  "horizontal_bar"
+)
+
+# No generic for floor method because of base R floor function
+for (e in event_names[-1]) {
+  eval(substitute(setGeneric(sn, function(x) standardGeneric(sn)), list(sn = e)))
+}
+
 get_event <- function(event) {
+  force(event)
   function(x) methods::slot(x, event)
 }
 
-# No generic for floor method because of base R floor function
-methods::setMethod("floor", "Competition", get_event("floor"))
+for (e in event_names) {
+  setMethod(e, "Competition", get_event(e))
+}
 
-methods::setGeneric("pommel_horse", def = function(x) standardGeneric("pommel_horse"))
-methods::setMethod("pommel_horse", "Competition", get_event("pommel_horse"))
-
-methods::setGeneric("still_rings", def = function(x) standardGeneric("still_rings"))
-methods::setMethod("still_rings", "Competition", get_event("still_rings"))
-
-methods::setGeneric("vault", def = function(x) standardGeneric("vault"))
-methods::setMethod("vault", "Competition", get_event("vault"))
-
-methods::setGeneric("parallel_bars", def = function(x) standardGeneric("parallel_bars"))
-methods::setMethod("parallel_bars", "Competition", get_event("parallel_bars"))
-
-methods::setGeneric("horizontal_bar", def = function(x) standardGeneric("horizontal_bar"))
-methods::setMethod("horizontal_bar", "Competition", get_event("horizontal_bar"))
-
-
-methods::setGeneric("floor<-", def = function(x, value) standardGeneric("floor<-"))
-methods::setMethod(
-  "floor<-", "Competition",
+set_event <- function(event) {
+  force(event)
   function(x, value) {
-    home_starts(value) <- home_starts(floor(x))
-    methods::slot(x, "floor") <- value
+    getter <- get(event)
+    home_starts(value) <- home_starts(getter(x))
+    methods::slot(x, event) <- value
     methods::validObject(x)
     x
   }
-)
+}
 
-methods::setGeneric("pommel_horse<-", def = function(x, value) standardGeneric("pommel_horse<-"))
-methods::setMethod(
-  "pommel_horse<-", "Competition",
-  function(x, value) {
-    home_starts(value) <- home_starts(pommel_horse(x))
-    methods::slot(x, "pommel_horse") <- value
-    methods::validObject(x)
-    x
-  }
-)
+for (e in event_names) {
+  f <- paste0(e, "<-")
+  eval(substitute(setGeneric(sn, function(x, value) standardGeneric(sn)), list(sn = f)))
+}
 
-methods::setGeneric("still_rings<-", def = function(x, value) standardGeneric("still_rings<-"))
-methods::setMethod(
-  "still_rings<-", "Competition",
-  function(x, value) {
-    home_starts(value) <- home_starts(still_rings(x))
-    methods::slot(x, "still_rings") <- value
-    methods::validObject(x)
-    x
-  }
-)
-
-methods::setGeneric("vault<-", def = function(x, value) standardGeneric("vault<-"))
-methods::setMethod(
-  "vault<-", "Competition",
-  function(x, value) {
-    home_starts(value) <- home_starts(vault(x))
-    methods::slot(x, "vault") <- value
-    methods::validObject(x)
-    x
-  }
-)
-
-methods::setGeneric("parallel_bars<-", def = function(x, value) standardGeneric("parallel_bars<-"))
-methods::setMethod(
-  "parallel_bars<-", "Competition",
-  function(x, value) {
-    home_starts(value) <- home_starts(parallel_bars(x))
-    methods::slot(x, "parallel_bars") <- value
-    methods::validObject(x)
-    x
-  }
-)
-
-methods::setGeneric("horizontal_bar<-", def = function(x, value) standardGeneric("horizontal_bar<-"))
-methods::setMethod(
-  "horizontal_bar<-", "Competition",
-  function(x, value) {
-    home_starts(value) <- home_starts(horizontal_bar(x))
-    methods::slot(x, "horizontal_bar") <- value
-    methods::validObject(x)
-    x
-  }
-)
+for (e in event_names) {
+  f <- paste0(e, "<-")
+  setMethod(f, "Competition", set_event(e))
+}
