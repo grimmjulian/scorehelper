4624f9c9c4af5acbce9714af407990d1cd594efa
Julian Grimm <grimmjulian225@gmail.com>
2026-01-21 04:53:03 +0100

    Split pairing_mod into teams

--- R/mod_pairing.R
+++ R/mod_pairing.R
@@ -1,36 +1,45 @@
 #' @include pairing.R mod_routine.R
 
-pairing_layout <- function(home_routine, home_score, guest_routine, guest_score) {
-	routine_width <- 5
-	score_width <- (12 - 2 * routine_width) / 2
+pairing_team_ui <- function(id) {
+	ns <- shiny::NS(id)
+	routine_width <- 10
+	score_width <- 12 - routine_width
 	shiny::tagList(
 		shiny::fluidRow(
-			shiny::column(routine_width, home_routine),
-			shiny::column(score_width, home_score),
-			shiny::column(routine_width, guest_routine),
-			shiny::column(score_width, guest_score)
+			shiny::column(routine_width, routine_ui(ns("routine"))),
+			shiny::column(score_width, shiny::textOutput(ns("score")))
 		)
 	)
 }
 
+pairing_team_server <- function(id, p) {
+	team_index <- 2
+	if (endsWith(id, "home")) {
+		team_index <- 1
+	}
+	shiny::moduleServer(id, function(input, output, session) {
+		r <- routine_server("routine")
+		output$score <- shiny::renderText(p()@score[[team_index]])
+		return(r)
+	})
+}
+
 pairing_ui <- function(id) {
 	ns <- shiny::NS(id)
-	pairing_layout(
-		routine_ui(ns("home")),
-		shiny::textOutput(ns("home_score")),
-		routine_ui(ns("guest")),
-		shiny::textOutput(ns("guest_score"))
+	shiny::tagList(
+		shiny::fluidRow(
+			shiny::column(6, pairing_team_ui(ns("home"))),
+			shiny::column(6, pairing_team_ui(ns("guest")))
+		)
 	)
 }
 
 pairing_server <- function(id) {
 	shiny::moduleServer(id, function(input, output, session) {
-		home <- routine_server("home")
-		guest <- routine_server("guest")
+		home <- pairing_team_server("home", p)
+		guest <- pairing_team_server("guest", p)
 		p <- pairing(home = home(), guest = guest()) |> # nolint: object_usage_linter.
 			shiny::reactive()
-		output$home_score <- shiny::renderText(p()@score[[1]])
-		output$guest_score <- shiny::renderText(p()@score[[2]])
 		return(p)
 	})
 }
